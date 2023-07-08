3abd7cb07701660e50c486624326a7856b1b39f3
Patrick Han <patrick-han@users.noreply.github.com>
2023-07-07 20:20:54 -0700

    pulled out vertex and index buffer creation into a generic buffer creation function

--- include/buffer.h
+++ include/buffer.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "vulkan/vulkan.hpp"
 #include "vk_mem_alloc.h"
+#include "types.h"
 
 struct AllocatedBuffer {
     vk::Buffer buffer;
     VmaAllocation allocation;
 };
+
+
+void upload_buffer(AllocatedBuffer& allocatedBuffer, size_t bufferSize, const void* bufferData, VkBufferUsageFlags bufferUsage, VmaAllocator allocator, DeletionQueue* deletionQueue);
