163b03a94a029aea605e60404047bc61efa59fcd
Patrick Han <patrick-han@users.noreply.github.com>
2023-09-07 22:26:26 -0700

    pulled out vertex and index buffer creation into a generic buffer creation function

---
+++ src/buffer.cpp
@@ -0,0 +1,35 @@
+#include "buffer.h"
+
+void upload_buffer(AllocatedBuffer& allocatedBuffer, size_t bufferSize, const void* bufferData, VkBufferUsageFlags bufferUsage, VmaAllocator allocator, DeletionQueue* deletionQueue) {
+    // Create buffer
+    VkBufferCreateInfo bufferCreateInfo = {};
+    bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferCreateInfo.size = bufferSize;
+    bufferCreateInfo.usage = bufferUsage;
+
+    // Let the VMA library know that this data should be writeable by CPU, but also readable by GPU
+    VmaAllocationCreateInfo vmaAllocInfo = {};
+    vmaAllocInfo.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+
+    // Allocate the buffer
+    VkResult res = vmaCreateBuffer(allocator, &bufferCreateInfo, &vmaAllocInfo,
+        &reinterpret_cast<VkBuffer &>(allocatedBuffer.buffer), // bleck, but apparently "vulkan.hpp contains static_asserts that ensure that vk::Buffer and VkBuffer are the same size" so this is okay
+        &allocatedBuffer.allocation,
+        nullptr
+    );
+    if (res != VK_SUCCESS) {
+        MRCERR("Failed to allocate buffer!");
+        exit(0);
+    }
+    
+    // Add the destruction of mesh buffer to the deletion queue
+    deletionQueue->push_function([=]() {
+        vmaDestroyBuffer(allocator, allocatedBuffer.buffer, allocatedBuffer.allocation);
+    });
+
+    // Copy vertex data into mapped memory
+    void* data;
+    vmaMapMemory(allocator, allocatedBuffer.allocation, &data);
+    memcpy(data, bufferData, bufferSize);
+    vmaUnmapMemory(allocator, allocatedBuffer.allocation);
+}
