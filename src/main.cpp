e2927b4f94dae7269e4a0dd9f000437b79815078
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2023-09-07 22:24:06 -0700

    restructure main

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,72 @@
 
 #include <iostream>
 #include <vector>
+#include <stdexcept>
+#include <cstdlib>
 #include "RootDir.h"
 
-int main() {
-    glfwInit();
+const uint32_t WINDOW_WIDTH = 800;
+const uint32_t WINDOW_HEIGHT = 600;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vk::Result a = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-    
-    std::cout << ROOT_DIR << std::endl;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+class Engine {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    glfwDestroyWindow(window);
+private:
+    GLFWwindow* window;
 
-    glfwTerminate();
 
-    return 0;
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan Engine", nullptr, nullptr);
+
+    }
+
+    void initVulkan() {
+
+    }
+
+    void mainLoop() {
+        uint32_t extensionCount = 0;
+        vk::Result a = vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::cout << extensionCount << " extensions supported\n";
+
+        glm::mat4 matrix;
+        glm::vec4 vec;
+        auto test = matrix * vec;
+        
+        std::cout << ROOT_DIR << std::endl;
+
+
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    Engine engine;
+
+    try {
+        engine.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
