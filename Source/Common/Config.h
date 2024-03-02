d606b1a4914d1fc8edc26ec031de68c0f3c97aa5
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2024-03-02 09:15:32 -0800

    Add temporary Windows specific resolution

--- Source/Common/Config.h
+++ Source/Common/Config.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <cstdlib>
+#include <Common/Platform.h>
 
+#if PLATFORM_WINDOWS
+inline constexpr uint32_t WINDOW_WIDTH = 2500;
+inline constexpr uint32_t WINDOW_HEIGHT = 1200;
+#else
 inline constexpr uint32_t WINDOW_WIDTH = 1500;
 inline constexpr uint32_t WINDOW_HEIGHT = 800;
+#endif
 inline constexpr int MAX_FRAMES_IN_FLIGHT = 2;
