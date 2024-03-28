fb3b193b244974c0b617907dee01395f21b49438
Patrick Han <patrick-han@users.noreply.github.com>
2024-03-27 23:49:56 -0700

    update SurfaceHelper.mm comments

--- Source/SurfaceHelper.mm
+++ Source/SurfaceHelper.mm
@@ -3,9 +3,13 @@
 #include <Common/Compiler/DisableWarnings.h>
 PUSH_CLANG_WARNINGS
 DISABLE_CLANG_WARNING("-Wgnu-statement-expression-from-macro-expansion")
+
+// This code (and comment) are referenced from Diligent Samples, just replace glfw with SDL wordage. Kept here for posterity:
+// "
 // MoltenVk implement Vulkan API on top of Metal.
 // We must add CAMetalLayer to use Metal to render into view.
 // Same code implemented into glfwCreateWindowSurface, but DiligentEngine create vulkan surface inside a ISwapChain implementation and can not use the GLFW function.
+// "
 void* GetNSWindowView(SDL_Window* wnd)
 {
     id Window = (NSWindow *)SDL_GetProperty(SDL_GetWindowProperties(wnd), SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, NULL);
